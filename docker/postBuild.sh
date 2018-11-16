797eb467500afec2d0b08484037c54a91395bc04
Pasquale Antonante <antonap@mit.edu>
2018-11-16 11:04:09 -0500

    Improve smmetry section, enable auto initialization of the notebook, fix some autoupdate cells

--- docker/postBuild.sh
+++ docker/postBuild.sh
@@ -4,6 +4,6 @@ jupyter contrib nbextension install --user
 jupyter nbextension install --py --user latex_envs
 jupyter nbextension enable --py widgetsnbextension
 jupyter nbextension enable python-markdown/main
-jupyter nbextension enable limit_output/main
 jupyter nbextension enable toc2/main
+jupyter nbextension enable init_cell/main
 jupyter nbextension enable --py latex_envs
